1491fa0d7c6e51f9c999bf26166e7ba192f0dcfb
Alexander <alxndr+github@gmail.com>
2023-06-27 22:47:51 -0700

    whoops

--- assets/javascripts/discourse/initializers/setlist.js
+++ assets/javascripts/discourse/initializers/setlist.js
@@ -1,4 +1,6 @@
 import { withPluginApi } from 'discourse/lib/plugin-api';
+import ComposerController from 'discourse/controllers/composer';
+import { addTagDecorateCallback } from 'discourse/lib/to-markdown';
 
 function renderSetlist(...args) {
   console.log('we did it', ...args);
@@ -14,12 +16,18 @@ export function initializeSetlistCode(api) {
           '[setlist]',
           '[/setlist]',
           'setlist_text',
-          { multiline: false, useBlockMode: true }
+          { multiline: false, useBlockMode: false }
         );
       },
     },
   });
 
+  addTagDecorateCallback(function () {
+    if (this.element.attributes.class === 'setlist') {
+      this.prefix = '[setlist]';
+      this.suffix = '[/setlist]';
+    }
+  });
 
   // decorate "cooked" content (after it's been rendered)
   // https://github.com/discourse/discourse/blob/1526d1f97d46/app/assets/javascripts/discourse/app/lib/plugin-api.js#L369
