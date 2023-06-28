b0de4990dbf3f2df62000dc46a16aa44930ab3c4
Alexander <alxndr+github@gmail.com>
2023-06-27 21:31:38 -0700

    version un-bump

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-plugin-gizzard-setlist
 # about: Turn a [setlist] code into a King Gizz setlist.
-# version: 1.0.0-alpha
+# version: 0.1.0-alpha
 # authors: Axe
 # url: https://github.com/kglw-dot-net/discourse-plugin-gizzard-setlist
 
