b2c89939a8541865d4a1464f0d136f1a821805d4
Alexander <alxndr+github@gmail.com>
2023-07-08 11:52:02 -0700

    more callbacks
    0.2.0-wip15

--- assets/javascripts/discourse/initializers/setlist.js
+++ assets/javascripts/discourse/initializers/setlist.js
@@ -1,36 +1,28 @@
 import { withPluginApi } from 'discourse/lib/plugin-api';
 import ComposerController from 'discourse/controllers/composer';
-import { addBlockDecorateCallback } from 'discourse/lib/to-markdown';
+import { addBlockDecorateCallback, addTagDecorateCallback } from 'discourse/lib/to-markdown';
 
-const PLUGIN_NAME = 'setlist';
+const PLUGIN_NAME = 'setlist'; // TBD Does this have to match the filename?
 const HTML_CLASS_NAME = 'kglwSetlist';
-const HTML_CLASS_NAME_PROCESSED = 'kglwSetlistProcessed';
+const HTML_CLASS_NAME_PROCESSED = 'kglwSetlistServed';
+const REGEX_DATE_FORMAT = /^(\d{4})-(\d{2})-(\d{2})(?!#(\d))?$/ // TODO check date format... ...set class `kglwSetlist-invalid` if no good
+
+function log(...msgs) {
+  console.log('%cKGLW', 'color:chartreuse;background:black;padding:0.2rem;border-radius:1rem', ...msgs)
+}
 
 async function doTheSetlist(setlistElement) {
-  global.console.log('do the setlist!', fetch, setlistElement);
+  log('do the setlist!', fetch, setlistElement);
   if (!fetch)
     return console.error('no fetch...');
   setlistElement.classList.remove(HTML_CLASS_NAME);
   const date = setlistElement.innerHTML;
   const showData = (await (await fetch(`https://kglw.net/api/v1/shows/showdate/${date}.json`)).json()).data
   const setlistData = (await (await fetch(`https://kglw.net/api/v1/setlists/showdate/${date}.json`)).json()).data
-  global.console.log({showData, setlistData});
+  log('doTheSetlist', {showData, setlistData});
   const setlist = setlistData.map(song => `${song.songname}${song.transition}`).join('')
   setlistElement.attributes.title = `${showData.showdate} @ ${showData.venuename}\n\n${setlist}`
   setlistElement.classList.add(HTML_CLASS_NAME_PROCESSED);
-}
-
-function renderSetlist(cookedElement) {
-  cookedElement.querySelectorAll(`.${HTML_CLASS_NAME}`).forEach((elem) => {
-    global.console.log('renderSetlist', elem);
-    elem.addEventListener('click', () => {
-      doTheSetlist(elem);
-    });
-    elem.addEventListener('keydown', ({key}) => {
-      if (key === 'Enter')
-        doTheSetlist(elem);
-    });
-  });
 }
 
 export function initializeSetlistCode(api) {
@@ -39,30 +31,53 @@ export function initializeSetlistCode(api) {
   ComposerController.reopen({
     actions: {
       insertSetlist() { // matches action prop passed to addToolbarPopupMenuOptionsCallback
-        // TODO check date format...
-        console.log('insertSetlist...', this, ...arguments);
         this.get('toolbarEvent').applySurround(
           '[setlist]',
           '[/setlist]',
           'setlist_text', // locale string js.composer.setlist_text
           { multiline: false, useBlockMode: false }
         );
+        log('insertSetlist...', this, ...arguments);
       },
     },
   });
 
-  addBlockDecorateCallback(function (text) {
-    global.console.log('add callback...', text, this.element, [...this.element.classList]);
+  // convert nodes back to bbcode
+  addTagDecorateCallback(function (text) {
+    log('addTagDecorateCallback', text, this.element);
     if ([...this.element.classList].includes(HTML_CLASS_NAME)) {
       this.prefix = '[setlist]';
       this.suffix = '[/setlist]';
-      return this.text.trim();
+    }
+  });
+
+  addBlockDecorateCallback(function (text) {
+    log('addBlockDecorateCallback', text, this.element);
+    const { name, attributes } = this.element;
+    if (name === 'div' && attributes.class === HTML_CLASS_NAME_PROCESSED) {
+      this.prefix = '[setlist]';
+      this.suffix = '[/setlist]';
+      return text.trim();
     }
   });
 
   // decorate "cooked" content (after it's been rendered)
   // https://github.com/discourse/discourse/blob/1526d1f97d46/app/assets/javascripts/discourse/app/lib/plugin-api.js#L369
-  api.decorateCookedElement(renderSetlist, { id: PLUGIN_NAME }); // what's this ID for?
+  api.decorateCookedElement(function(cookedElement) {
+    cookedElement.querySelectorAll(`.${HTML_CLASS_NAME}`).forEach((elem) => {
+      log('decorateCookedElement...', elem);
+      elem.addEventListener('click', () => {
+        doTheSetlist(elem);
+      });
+      elem.addEventListener('keydown', ({key}) => {
+        if (key === 'Enter')
+          doTheSetlist(elem);
+      });
+    });
+  }, {
+    afterAdopt: true, // decorate html content after it is adopted by the main `document` (not in a detached DOM)
+    id: HTML_CLASS_NAME,
+  });
 }
 
 export default {
--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-plugin-gizzard-setlist
 # about: Turn a [setlist] code into a King Gizz setlist.
-# version: 0.2.0-wip14
+# version: 0.2.0-wip15
 # authors: Axe
 # url: https://github.com/kglw-dot-net/discourse-plugin-gizzard-setlist
 
