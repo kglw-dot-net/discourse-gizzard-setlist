aeffcf0e3858e1052c9463696a629eba264805d9
Alexander <alxndr+github@gmail.com>
2023-07-11 20:23:40 -0700

    register_asset for scss
    0.2.0-wip23

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,8 @@
 # name: discourse-plugin-gizzard-setlist
 # about: Turn a [setlist] code into a King Gizz setlist.
-# version: 0.2.0-wip22
+# version: 0.2.0-wip23
 # authors: Axe
 # url: https://github.com/kglw-dot-net/discourse-plugin-gizzard-setlist
 
 enabled_site_setting :kglwSetlist_enabled
+register_asset 'stylesheets/discourse_kglwSetlist.scss'
