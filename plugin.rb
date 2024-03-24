66c9be434a9b24836bc3fdf20a2455482508c533
Alexander <alxndr+github@gmail.com>
2024-03-24 10:31:42 -0700

    ops: adjust plugin name

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,4 @@
-# name: discourse-plugin-gizzard-setlist
+# name: gizzard-setlist
 # about: Turn a [setlist] code into a King Gizz setlist: `[setlist]2022-10-02[/setlist]` use `#2` to specify second show on same date `[setlist]2023-06-08#2[/setlist]`
 # version: 0.2.3-alpha
 # authors: KGLW.net, Axe <alxndr+kglw-setlist-plugin@gmail.com>
