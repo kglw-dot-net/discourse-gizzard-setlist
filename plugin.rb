ea2426ed47fa08d5b78614917f478d0a2e229cf4
Alexander <alxndr+github@gmail.com>
2024-03-31 11:33:31 -0700

    version: bump to alpha18
    Discourse thinks it's on 17 but the JS looks like 16...

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: gizzard-setlist
 # about: Turn a [setlist] code into a King Gizz setlist popup.
-# version: 0.2.3-alpha17
+# version: 0.2.3-alpha18
 # authors: Axe <alxndr+kglw-setlist-plugin@gmail.com> & KGLW.net
 # url: https://github.com/kglw-dot-net/discourse-plugin-gizzard-setlist
 
