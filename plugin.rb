83d2f5c72b0ef7ad319de000b0f746d0d52c596b
Alexander <alxndr+github@gmail.com>
2024-03-31 17:17:50 -0700

    docs: update URLs

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 # about: Turn a [setlist] code into a King Gizz setlist popup.
 # version: 0.2.3-alpha18
 # authors: Axe <alxndr+kglw-setlist-plugin@gmail.com> & KGLW.net
-# url: https://github.com/kglw-dot-net/discourse-plugin-gizzard-setlist
+# url: https://github.com/kglw-dot-net/discourse-gizzard-setlist
 
 enabled_site_setting :kglwSetlist_enabled # setting name matches field in kglw-setlist.js
 register_asset 'stylesheets/discourse_kglwSetlist.scss'
